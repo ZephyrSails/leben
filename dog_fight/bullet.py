7f83d5a77d4f58ba6bdb4ad2001cfa3839f36797
ZephyrSails <zephyrsails@gmail.com>
2019-11-03 15:27:38 -0800

    randomed & bouncing  bullet

--- dog_fight/bullet.py
+++ dog_fight/bullet.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -13,10 +14,10 @@ class Bullet(pygame.sprite.Sprite):
         self.SCREEN_HEIGHT = flight.SCREEN_HEIGHT
 
         # motion
-        self.speed_multiplier = 4
-        self.radians = flight.dir_radians
-        self.x_speed = math.cos(self.radians) * self.speed_multiplier
-        self.y_speed = math.sin(self.radians) * self.speed_multiplier
+        self.speed = 10
+        self.radians = math.radians(flight.dir_degree + random.randint(-2, 2))
+        self.x_speed = math.cos(self.radians) * self.speed
+        self.y_speed = math.sin(self.radians) * self.speed
 
         # attribute
         self.radius = 2
@@ -39,20 +40,22 @@ class Bullet(pygame.sprite.Sprite):
         ))
 
         # life span
-        self.life_tick = 100
+        self.life_tick = 200
 
         # explosion visual
         self.to_be_killed_in_n_turn = -1
 
     def draw(self):
-        pygame.draw.circle(self.surf, self.bg_color,
+        pygame.draw.circle(self.surf, self.color,
                            (self.bound_radius, self.bound_radius), self.radius)
-        pygame.draw.line(
-            self.surf, self.color,
-            (int(self.bound_radius - self.radius * math.cos(self.radians)),
-             int(self.bound_radius - self.radius * math.sin(self.radians))),
-            (int(self.bound_radius + self.radius * math.cos(self.radians)),
-             int(self.bound_radius + self.radius * math.sin(self.radians))), 1)
+        # pygame.draw.circle(self.surf, self.bg_color,
+        #                    (self.bound_radius, self.bound_radius), self.radius)
+        # pygame.draw.line(
+        #     self.surf, self.color,
+        #     (int(self.bound_radius - self.radius * math.cos(self.radians)),
+        #      int(self.bound_radius - self.radius * math.sin(self.radians))),
+        #     (int(self.bound_radius + self.radius * math.cos(self.radians)),
+        #      int(self.bound_radius + self.radius * math.sin(self.radians))), 1)
 
     def update(self):
         if self.to_be_killed_in_n_turn != -1:
@@ -71,13 +74,17 @@ class Bullet(pygame.sprite.Sprite):
             self.rect.move_ip(dx, dy)
 
             if self.rect.left < 0:
-                self.rect.move_ip(self.SCREEN_WIDTH, 0)
+                self.x_speed = -self.x_speed
+                # self.rect.move_ip(self.SCREEN_WIDTH, 0)
             if self.rect.left > self.SCREEN_WIDTH:
-                self.rect.move_ip(-self.SCREEN_WIDTH, 0)
+                self.x_speed = -self.x_speed
+                # self.rect.move_ip(-self.SCREEN_WIDTH, 0)
             if self.rect.top <= 0:
-                self.rect.move_ip(0, self.SCREEN_HEIGHT)
+                self.y_speed = -self.y_speed
+                # self.rect.move_ip(0, self.SCREEN_HEIGHT)
             if self.rect.top >= self.SCREEN_HEIGHT:
-                self.rect.move_ip(0, -self.SCREEN_HEIGHT)
+                self.y_speed = -self.y_speed
+                # self.rect.move_ip(0, -self.SCREEN_HEIGHT)
 
         self.life_tick -= 1
         if self.life_tick == 0:
